67b7d84cda90c82a037a3c16f4761be9421c72cb
ChelovechekAD <yushkevich2004@mail.ru>
2021-12-02 01:40:20 +0300

    Небольшие правки, доп инф прилагается
    Правки сделанные в течении доп. 10 минут, дабы прям не наглеть. Я долго думал оставлять ли их, тк за ограничение по времени я вышел, но я их оставлю. Что проверять решать вам

---
+++ Задание2 Извините, подробно в коммите/scripts/script.js
@@ -0,0 +1,6 @@
+$(window).resize(()=>{$('#img1_block_3').height($('#img2_block_3').height())});
+$(document).ready(()=>{$('#img1_block_3').height($('#img2_block_3').height())});
+$(window).resize(()=>{$('#img2_block_5').height($('#img1_block_5').height())});
+$(document).ready(()=>{$('#img2_block_5').height($('#img1_block_5').height())});
+$(window).resize(()=>{$('#img3_block_5').height($('#img1_block_5').height())});
+$(document).ready(()=>{$('#img3_block_5').height($('#img1_block_5').height())});
